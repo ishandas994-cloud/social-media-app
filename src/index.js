8ca17c3a82e2b8d835d4ef1dcf792c56da282287
ishandas994-cloud <ishandas994@gmail.com>
2026-05-08 09:43:57 +0530

    new codew

--- src/index.js
+++ src/index.js
@@ -15,7 +15,14 @@ dotenv.config({
 connectDB()
 
 
-
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`server  is running at post  :${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("mongodb connection error",err)
+})
 
 
 
