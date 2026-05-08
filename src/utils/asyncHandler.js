d4ecee7bc2d9adfbcf48feb350dc5e7ca49a4dd4
ishandas994-cloud <ishandas994@gmail.com>
2026-05-08 10:12:07 +0530

    new code

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,20 @@
-const asyncHandler =()=>{
+// const asyncHandler =(fn)=>async(req,res,next)=>{
+//     try{
 
+//     }catch(error){
+//         res.status(error.code||500).json({
+//             success:false,
+//             message:error.message
+//         })
+//     }
+// }
+
+
+const asyncHandler =(requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+    }
 }
+
 
 export {asyncHandler}
