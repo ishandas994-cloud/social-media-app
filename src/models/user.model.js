c57c6a8107d08d14f5bff17fad7144ee6c494295
ishandas994-cloud <ishandas994@gmail.com>
2026-05-09 11:36:43 +0530

    new code

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -53,4 +53,34 @@ userSchema.pre("save",async function(next) {
 userSchema.methods.isPasswordCorrect =async function(password) {
     return await bcrypt.compare(password,this.password)
 }
+
+
+userSchema.methods.generateAccessToken=function(){
+  return  jwt.sign({
+        _id:this._id,
+        email:this.email,
+        username:this.fullname,
+        fullname:this.fullname
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+userSchema.methods.generateRefraceToken=function(){
+    return  jwt.sign({
+        _id:this._id,
+        email:this.email,
+        username:this.fullname,
+        fullname:this.fullname
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+)
+}
+
+
 export const user = mongoose.model("user", userSchema)
